2e9c1361e06b5ffbf6e3cad4667cdb3448dd85c0
adriannaxx95 <153696606+adriannaxx95@users.noreply.github.com>
2024-02-17 14:25:27 -0500

    Add files via upload

---
+++ JS Intermediate Lab/index.js
@@ -0,0 +1,14 @@
+// 1. will go here
+
+const hello = "'hello how are you"
+console.log(hello.toUpperCase())
+console.log(hello.toLowerCase())
+console.log(hello.endsWith('how are you'))
+console.log(ucFirstLetters("hello how are you"))
+
+
+// 2. will go here
+
+// 3.
+
+
